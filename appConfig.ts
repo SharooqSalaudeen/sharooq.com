9fa886a25e30d379c78225221da8a7684cd60ac5
Sharooq Salaudeen <coolsharooqs@gmail.com>
2021-07-04 16:56:18 +0530

    optimized by removing feature images

--- appConfig.ts
+++ appConfig.ts
@@ -21,7 +21,7 @@ export const sourceImages: boolean = false
 export const rssFeed: boolean = true
 
 // Ghost Member Subscriptions
-export const memberSubscriptions: boolean = true
+export const memberSubscriptions: boolean = false
 
 // Commenting system
 export type CommentingSystem = 'commento' | 'disqus' | null
